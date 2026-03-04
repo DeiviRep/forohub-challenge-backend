154b44bcd158d33ccfcb8584e20e86275fe07c30
BC3264 <BC3264@WPS17117001>
2026-03-04 10:23:57 -0400

    feat: add DELETE /topicos/{id}

--- src/main/java/com/alura/foro_hub/controller/TopicoController.java
+++ src/main/java/com/alura/foro_hub/controller/TopicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,6 +73,13 @@ public class TopicoController {
     public ResponseEntity<DatosDetalleTopico> actualizar(@PathVariable Long id, @RequestBody @Valid DatosUpdateTopico datos) {
         var result = topicoService.actualizar(id, datos);
         return ResponseEntity.ok(result);
+    }
+    
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        topicoService.eliminar(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
